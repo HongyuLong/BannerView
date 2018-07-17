3553c28a3a0f24ab6ce1feda24b841ea305e40b1
longhongyu <1372677064@qq.com>
2018-07-17 22:18:22 +0800

    versionBylonghongyu

--- app/src/main/java/com/example/longhongyu/bannerview/userhome/FlyerOfClassificationActivity.java
+++ app/src/main/java/com/example/longhongyu/bannerview/userhome/FlyerOfClassificationActivity.java
@@ -1,9 +1,15 @@
 package com.example.longhongyu.bannerview.userhome;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class FlyerOfClassificationActivity extends AppCompatActivity{
 
 
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+
+    }
 
 }
